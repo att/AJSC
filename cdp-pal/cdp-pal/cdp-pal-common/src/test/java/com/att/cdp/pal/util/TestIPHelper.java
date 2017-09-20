e72a9687cf012c340f466451916397b1c1169131
DAS <sd072k@att.com>
2017-09-20 17:28:18 -0500

    CDP PAL Defect by AppC team for mechid passwords are visible in logs

--- cdp-pal/cdp-pal/cdp-pal-common/src/test/java/com/att/cdp/pal/util/TestIPHelper.java
+++ cdp-pal/cdp-pal/cdp-pal-common/src/test/java/com/att/cdp/pal/util/TestIPHelper.java
@@ -7,6 +7,8 @@ package com.att.cdp.pal.util;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 
