a5401128f04be43c44531b43a3778334297ed14b
Amit Patel <ap120m@att.com>
2018-03-26 09:52:25 -0400

    [ALTS-421] Added reboot API method to support the Reboot VM flow-Added Unit Tests

--- cdp-pal/cdp-pal/cdp-pal-openstack/src/test/java/com/att/cdp/openstack/v1/TestComputeService.java
+++ cdp-pal/cdp-pal/cdp-pal-openstack/src/test/java/com/att/cdp/openstack/v1/TestComputeService.java
@@ -10,8 +10,10 @@ import java.util.List;
 
 import org.junit.Ignore;
 import org.junit.Test;
+import org.omg.CORBA.DynAnyPackage.Invalid;
 
 import com.att.cdp.AbstractTestCase;
+import com.att.cdp.exceptions.InvalidRequestException;
 import com.att.cdp.exceptions.ZoneException;
 import com.att.cdp.zones.ComputeService;
 import com.att.cdp.zones.Context;
@@ -26,144 +28,194 @@ import com.att.cdp.zones.model.Subnet;
  */
 public class TestComputeService extends AbstractTestCase {
 
-    /**
-     * Test that we can list all ports on some server
-     * 
-     * @throws ZoneException
-     *             if anything fails
-     */
-    @Test
-    @Ignore
-    public void testListPorts() throws ZoneException {
-        Context context = connect();
-        ComputeService service = context.getComputeService();
+	/**
+	 * Test that we can list all ports on some server
+	 * 
+	 * @throws ZoneException
+	 *             if anything fails
+	 */
+	@Test
+	@Ignore
+	public void testListPorts() throws ZoneException {
+		Context context = connect();
+		ComputeService service = context.getComputeService();
 
-        List<Server> servers = service.getServers();
-        assertNotNull(servers);
-        assertFalse(servers.isEmpty());
+		List<Server> servers = service.getServers();
+		assertNotNull(servers);
+		assertFalse(servers.isEmpty());
 
-        for (Server server : servers) {
-            List<Port> ports = service.getPorts(server);
-            assertNotNull(ports);
-            assertFalse(ports.isEmpty());
+		for (Server server : servers) {
+			List<Port> ports = service.getPorts(server);
+			assertNotNull(ports);
+			assertFalse(ports.isEmpty());
 
-            List<Port> serverPorts = server.getPorts();
-            assertNotNull(serverPorts);
-            assertFalse(serverPorts.isEmpty());
+			List<Port> serverPorts = server.getPorts();
+			assertNotNull(serverPorts);
+			assertFalse(serverPorts.isEmpty());
 
-            assertTrue(ports.containsAll(serverPorts));
+			assertTrue(ports.containsAll(serverPorts));
 
-            // System.out.printf("Server %s has %d ports\n", server.getName(), ports.size());
-        }
-    }
+			// System.out.printf("Server %s has %d ports\n", server.getName(),
+			// ports.size());
+		}
+	}
 
-    /**
-     * Test the attach and detach of a port
-     * 
-     * @throws ZoneException
-     *             if the test fails
-     */
-    @SuppressWarnings("nls")
-    @Test
-    @Ignore
-    public void testAttachAndDetachPorts() throws ZoneException {
-        Context context = connect();
-        ComputeService compute = context.getComputeService();
-        NetworkService network = context.getNetworkService();
+	/**
+	 * Test the attach and detach of a port
+	 * 
+	 * @throws ZoneException
+	 *             if the test fails
+	 */
+	@SuppressWarnings("nls")
+	@Test
+	@Ignore
+	public void testAttachAndDetachPorts() throws ZoneException {
+		Context context = connect();
+		ComputeService compute = context.getComputeService();
+		NetworkService network = context.getNetworkService();
 
-        /*
-         * Get a list of servers. Make sure that at least one server exists. We'll just use the first one we find.
-         */
-        List<Server> servers = compute.getServers();
-        assertNotNull(servers);
-        assertFalse(servers.isEmpty());
-        Server server = servers.get(0);
+		/*
+		 * Get a list of servers. Make sure that at least one server exists.
+		 * We'll just use the first one we find.
+		 */
+		List<Server> servers = compute.getServers();
+		assertNotNull(servers);
+		assertFalse(servers.isEmpty());
+		Server server = servers.get(0);
 
-        /*
-         * Now, get the ports attached to that server. Make sure there is at least one port. We'll use the first one we
-         * find.
-         */
-        List<Port> ports = server.getPorts();
-        assertNotNull(ports);
-        assertFalse(ports.isEmpty());
+		//
 
-        /*
-         * Use the first port to model the creation of a new port. We'll use the same subnet.
-         */
-        Port modelPort = ports.get(0);
-        Subnet subnet = network.getSubnetById(modelPort.getSubnetId());
-        Port newPort = network.createPort(subnet);
-        assertNotNull(newPort);
-        assertNotNull(newPort.getMacAddr());
-        assertNotNull(newPort.getId());
+		/*
+		 * Now, get the ports attached to that server. Make sure there is at
+		 * least one port. We'll use the first one we find.
+		 */
+		List<Port> ports = server.getPorts();
+		assertNotNull(ports);
+		assertFalse(ports.isEmpty());
 
-        /*
-         * Save the new port's ID. We'll use that to test that it was actually attached and detached
-         */
-        String id = newPort.getId();
+		/*
+		 * Use the first port to model the creation of a new port. We'll use the
+		 * same subnet.
+		 */
+		Port modelPort = ports.get(0);
+		Subnet subnet = network.getSubnetById(modelPort.getSubnetId());
+		Port newPort = network.createPort(subnet);
+		assertNotNull(newPort);
+		assertNotNull(newPort.getMacAddr());
+		assertNotNull(newPort.getId());
 
-        /*
-         * Attach the port to the server. Then list all the ports and make sure the new port is included
-         */
-        server.attachPort(newPort);
-        ports = server.getPorts();
-        boolean found = false;
-        for (Port p : ports) {
-            if (p.getId().equals(id)) {
-                found = true;
-                break;
-            }
-        }
-        if (!found) {
-            fail("Port is not listed as attached to the server");
-        }
+		/*
+		 * Save the new port's ID. We'll use that to test that it was actually
+		 * attached and detached
+		 */
+		String id = newPort.getId();
 
-        /*
-         * Now detach the port and make sure that it is NOT listed
-         */
-        server.detachPort(newPort);
-        try {
-            Thread.sleep(1000L);
-        } catch (InterruptedException e) {
-            // ignore
-        }
-        ports = server.getPorts();
-        found = false;
-        for (Port p : ports) {
-            if (p.getId().equals(id)) {
-                found = true;
-                break;
-            }
-        }
-        if (found) {
-            fail("Port is still listed as attached to the server");
-        }
+		/*
+		 * Attach the port to the server. Then list all the ports and make sure
+		 * the new port is included
+		 */
+		server.attachPort(newPort);
+		ports = server.getPorts();
+		boolean found = false;
+		for (Port p : ports) {
+			if (p.getId().equals(id)) {
+				found = true;
+				break;
+			}
+		}
+		if (!found) {
+			fail("Port is not listed as attached to the server");
+		}
 
-        /*
-         * Finally, delete the new port
-         */
-        network.deletePort(newPort);
-    }
+		/*
+		 * Now detach the port and make sure that it is NOT listed
+		 */
+		server.detachPort(newPort);
+		try {
+			Thread.sleep(1000L);
+		} catch (InterruptedException e) {
+			// ignore
+		}
+		ports = server.getPorts();
+		found = false;
+		for (Port p : ports) {
+			if (p.getId().equals(id)) {
+				found = true;
+				break;
+			}
+		}
+		if (found) {
+			fail("Port is still listed as attached to the server");
+		}
 
-    @Ignore
-    @Test
-    public void testDeletePort() throws ZoneException {
-        String[] ids =
-            {
-                "0e1ae02a-f34e-4208-a031-ba251c9d6538", "7235a241-a5d1-473c-97e2-f3115e91d878",
-                "791b9689-7f19-469a-9759-154018315d4b", "958bbf73-7559-439d-bbad-94e67fc07263",
-                "b43449bc-a427-4838-a0ab-4fd544533677", "b94deec4-fdcc-41d9-ae93-8d0f746011d2"
-            };
+		/*
+		 * Finally, delete the new port
+		 */
+		network.deletePort(newPort);
+	}
 
-        Context context = connect();
-        ComputeService compute = context.getComputeService();
-        NetworkService network = context.getNetworkService();
+	@Ignore
+	@Test
+	public void testDeletePort() throws ZoneException {
+		String[] ids = { "0e1ae02a-f34e-4208-a031-ba251c9d6538",
+				"7235a241-a5d1-473c-97e2-f3115e91d878",
+				"791b9689-7f19-469a-9759-154018315d4b",
+				"958bbf73-7559-439d-bbad-94e67fc07263",
+				"b43449bc-a427-4838-a0ab-4fd544533677",
+				"b94deec4-fdcc-41d9-ae93-8d0f746011d2" };
 
-        for (String id : ids) {
-            Port port = network.getPort(id);
-            if (port != null) {
-                port.delete();
-            }
-        }
-    }
+		Context context = connect();
+		ComputeService compute = context.getComputeService();
+		NetworkService network = context.getNetworkService();
+
+		for (String id : ids) {
+			Port port = network.getPort(id);
+			if (port != null) {
+				port.delete();
+			}
+		}
+	}
+
+	@Ignore
+	@Test(expected = InvalidRequestException.class) 
+	public void testInvalidRebootType() throws ZoneException {
+
+		Context context = connect();
+		ComputeService compute = context.getComputeService();
+		// NetworkService network = context.getNetworkService();
+		List<Server> ids = compute.getServers();
+
+		
+
+			for (Server id : ids) {
+				compute.rebootServer(id, "SOFT1");
+			}
+		
+	}
+	
+	@Ignore	
+	public void testSoftRebootService() throws ZoneException {
+
+		Context context = connect();
+		ComputeService compute = context.getComputeService();
+		// NetworkService network = context.getNetworkService();
+		List<Server> servers = compute.getServers();
+		Server server = servers.get(0);
+		compute.rebootServer(server, "SOFT");
+		assertTrue(server.getStatus().equals("REBOOT"));
+		
+	}
+	
+	@Ignore	
+	public void testHardRebootService() throws ZoneException {
+
+		Context context = connect();
+		ComputeService compute = context.getComputeService();
+		// NetworkService network = context.getNetworkService();
+		List<Server> servers = compute.getServers();
+		Server server = servers.get(0);
+		compute.rebootServer(server, "HARD");
+		assertTrue(server.getStatus().equals("HARD_REBOOT"));
+		
+	}
 }
